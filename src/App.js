ed862dbc8b9aa8ffa8d01dd60d43323a919fa2b2
czarnikhil27 <czarnikhil27@gmail.com>
2023-02-23 21:43:21 +0545

    commit4

--- src/App.js
+++ src/App.js
@@ -1,73 +1,96 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Map from "./Map";
+import './App.css';
 
 function App() {
-  
-  const [location, setLocation] = useState({
-    lat: 0,
-    lng: 0,
-    coordinates: [],
-  });
-  const [finalLocation, setFinalLocation] = useState([]);
-  function onSubmitHandler(e) {
-    e.preventDefault();
-    setLocation((prev) => {
-      const newLocation = {
-        lat: parseFloat(prev.lat),
-        lng: parseFloat(prev.lng),
-      };
-      prev.coordinates.push(newLocation);
-      prev.lat=0;
-      prev.lng=0;
-      return prev;
-    });
+  const latRef = useRef(null);
+  const lngRef = useRef(null);
+  const [locations, setLocations] = useState([]);
+  const [isSend, setIsSend] = useState(false);
+  const [file, setFile] = useState(false);
 
+
+  const onSubmitHandler = e =>{
+    e.preventDefault();
+    console.log(locations);
+    const lat = latRef.current.value;
+    const lng = lngRef.current.value;
+    setLocations(prev=>{
+      const newLocation = [...prev];
+      newLocation.push({"lat": parseFloat(lat), "lng":parseFloat(lng)});
+      return newLocation;
+    })
+    latRef.current.value ="";
+    lngRef.current.value="";
+    
   }
+  const onSendLocationHandler = e =>{
+    e.preventDefault();
+    if(locations.length>=2)
+    {
+      setIsSend(prev=>!prev);
+    } 
+  }
+  const onFileHandler = e =>{
+    
+      setFile(prev=>!prev);
+  }
+  
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    if (file && file.type === "application/json") {
+      const reader = new FileReader();
+      reader.readAsText(file, "UTF-8");
+
+      reader.onload = (event) => {
+        const jsonData = JSON.parse(event.target.result);
+        setLocations(prev=>{
+          return [...prev, ...jsonData];
+        });
+      };
+    }
+  };
+
   return (
     <div>
-      {finalLocation.length <= 0 && (
-        <form onSubmit={onSubmitHandler}>
-          <input
-            type="text"
-            name="lat"
-            placeholder="latidude"
-            value={location.lat}
-            onChange={(e) => {
-              setLocation((prev) => {
-                const newLocation = { ...prev };
-                newLocation.lat = e.target.value;
-                return newLocation;
-              });
-            }}
-          ></input>
-          <input
-            type="text"
-            name="lng"
-            placeholder="longitude"
-            value={location.lng}
-            onChange={(e) => {
-              setLocation((prev) => {
-                const newLocation = { ...prev };
-                newLocation.lng = e.target.value;
-                return newLocation;
-              });
-            }}
-          ></input>
-          <input type="submit" value="Add Location"></input>
-        </form>
-      )}
-      {location.coordinates.length >= 2 && finalLocation.length <= 0 && (
-        <button
-          onClick={(e) => {
-            setFinalLocation(location.coordinates);
-          }}
-        >
-          submit
-        </button>
-      )}
-      {finalLocation.length >= 1 && <Map locations={finalLocation} />}
+      { isSend === false &&
+      <div className="toggle-btn" >
+      <span className="toggle-btn-left"></span>
+      <label className="toggle-btn-switch">
+        <input type="checkbox" onClick={onFileHandler} />
+        <span className="toggle-btn-slider"></span>
+      </label>
+      <span className="toggle-btn-right">Upload File</span>
+    </div> }
+    <br></br>
+   { file === true && isSend===false &&
+   <>
+    <form>
+    <input type="file" accept=".json" onChange={handleFileUpload} />
+    </form>
+    <br></br>
+     <button onClick={onSendLocationHandler}>Submit</button>
+     </>
+    }
+
+   {
+   file===false && <>
+    {isSend===false && <form onSubmit={onSubmitHandler}>
+        <input type="text" placeholder="Latitude" ref={latRef} required></input>
+        <input type="text" placeholder="Longitute" ref={lngRef} required></input>
+        <input type="submit" value="Add the location"></input>
+      </form>}
+      <br></br>
+      {isSend===false && locations.length>=2 && <button onClick={onSendLocationHandler}>Send to Google Map</button>}
+      <br></br>
+
+      
+    </>
+    }
+    {isSend===true && locations.length>=2 && <Map locations={locations} />}
+      
     </div>
   );
-}
 
+}
 export default App;
--- src/Map.js
+++ src/Map.js
@@ -4,10 +4,11 @@ function Map({ locations }) {
   const [map, setMap] = useState(null);
   const [markers, setMarkers] = useState([]);
   const [path, setPath] = useState(null);
+  const [isPaused, setIsPaused] = useState(false);
 
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = process.env.REACT_APP_SCRIPT;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyD2zm9evjJgAk_aihSvSUbVEG2FxM2JdYw`;
     script.onload = () => {
       if (window.google) {
         const { google } = window;
@@ -25,51 +26,100 @@ function Map({ locations }) {
         let j = 0;
         const pathCoordinates = [];
         const pathInterval = setInterval(() => {
-          pathCoordinates.push(locations[j]);
-          if (j === locations.length - 1) {
-            clearInterval(pathInterval);
+          if (!isPaused) {
+            pathCoordinates.push(locations[j]);
+            if (j === locations.length - 1) {
+              clearInterval(pathInterval);
 
-            // Create a new path and add it to the map
-            const newPath = new maps.Polyline({
-              path: pathCoordinates,
-              strokeColor: "#FF0000",
-              strokeOpacity: 1.0,
-              strokeWeight: 2,
-            });
-            newPath.setMap(newMap);
-            setPath(newPath);
-
-            // Add markers to the map with delay
-            let i = 0;
-            const newMarkers = [];
-            const bounds = new maps.LatLngBounds(); // create a new bounds object
-            const markerInterval = setInterval(() => {
-              const marker = new maps.Marker({
-                position: locations[i],
-                map: newMap,
-                animation: maps.Animation.DROP,
+              // Create a new path and add it to the map
+              const newPath = new maps.Polyline({
+                path: pathCoordinates,
+                strokeColor: "#FF0000",
+                strokeOpacity: 1.0,
+                strokeWeight: 2,
               });
-              newMarkers.push(marker);
-              setMarkers(newMarkers);
 
-              bounds.extend(marker.getPosition()); // extend the bounds to include the new marker's position
-              newMap.fitBounds(bounds); // auto zoom the map to fit all markers
+              newPath.setMap(newMap);
+              setPath(newPath);
+              // Animate drawing the path
+              let i = 0;
+              const pathDrawInterval = setInterval(() => {
+                if (!isPaused) {
+                  const lineSymbol = {
+                    path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+                    strokeOpacity: 0.8,
+                    strokeColor: "#0000FF",
+                    strokeWeight: 2,
+                    scale: 3,
+                  };
+                  const partialPath = new maps.Polyline({
+                    path: pathCoordinates.slice(0, i),
+                    strokeColor: "#FF0000",
+                    strokeOpacity: 1.0,
+                    strokeWeight: 2,
+                    icons: [
+                      {
+                        icon: lineSymbol,
+                        offset: "100%",
+                      },
+                    ],
+                  });
 
-              i++;
-              if (i === locations.length) {
-                clearInterval(markerInterval);
-              }
-            }, 1000);
+                  partialPath.setMap(newMap);
+                  i++;
+                  if (i > pathCoordinates.length) {
+                    clearInterval(pathDrawInterval);
+                  }
+                }
+              }, 30);
+
+              // Add markers to the map with delay
+              let k = 0;
+              const newMarkers = [];
+              const bounds = new maps.LatLngBounds(); // create a new bounds object
+              const markerInterval = setInterval(() => {
+                if (!isPaused) {
+                  const marker = new maps.Marker({
+                    position: locations[k],
+                    map: newMap,
+                    animation: maps.Animation.DROP,
+                  });
+                  newMarkers.push(marker);
+                  setMarkers(newMarkers);
+
+                  bounds.extend(marker.getPosition()); // extend the bounds to include the new marker's position
+                  newMap.fitBounds(bounds); // auto zoom the map to fit all markers
+
+                  k++;
+                  if (k === locations.length) {
+                    clearInterval(markerInterval);
+                  }
+                }
+              }, 1000);
+            }
+
+            j++;
           }
-
-          j++;
         }, 1000);
       }
     };
     document.body.appendChild(script);
-  }, [locations]);
+  }, [locations, isPaused]);
 
-  return <div id="map" style={{ height: "500px", width: "100%" }} />;
+  const handlePauseResumeClick = () => {
+    setIsPaused((prevIsPaused) => !prevIsPaused);
+  };
+
+  return (
+    <div>
+      <div id="map" style={{ height: "500px", width: "100%" }} />
+
+      <button onClick={handlePauseResumeClick}>{isPaused ? "Resume" : "Pause"}</button>
+    </div>
+  );
 }
 
 export default Map;
+
+
+
